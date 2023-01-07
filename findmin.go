93d623bf6a4f7c0e63cb72cb3e7a2624fd59660f
Adrian <astonge@gmail.com>
2023-01-06 22:34:57 -0800

    findMin

---
+++ findmin.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func findMin(n []int) {
+	var current_min = n[0]
+
+	for i := 0; i < len(n); i++ {
+		if (n[i] < current_min) {
+			current_min = n[i]
+		}
+	}
+	fmt.Println(current_min)
+}
+
+func main() {
+	findMin([]int {1,2,3})
+	findMin([]int {-10, 42, 18, 37, 99, -100})
+	findMin([]int {-1,-2,-3})
+}
